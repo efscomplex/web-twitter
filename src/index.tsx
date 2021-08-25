3b78e2e61d94e8779b86c5d681d855fbe9c46f19
edgar <edgar@MacBook-Air-de-edgar.local>
2021-08-25 21:27:51 +0200

    init

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,8 +9,6 @@ ReactDOM.render(
   document.getElementById('root'),
 );
 
-// Hot Module Replacement (HMR) - Remove this snippet to remove HMR.
-// Learn more: https://snowpack.dev/concepts/hot-module-replacement
 if (import.meta.hot) {
   import.meta.hot.accept();
 }
